8e6ff15619c12d241c93f4227064970cb2cd0b50
Nithin Ravi <nithinravi2bcommb@gmail.com>
2024-02-22 11:07:37 +0530

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function showcollapse(){
+    document.getElementById("collapsediv").style.display="block";
+    document.getElementById("menuimg").style.display="none";
+    document.getElementById("dropimg").style.display="block";
+}
+
+function hidecollapse(){
+    document.getElementById("collapsediv").style.display="none";
+    document.getElementById("menuimg").style.display="block";
+    document.getElementById("dropimg").style.display="none";
+}
